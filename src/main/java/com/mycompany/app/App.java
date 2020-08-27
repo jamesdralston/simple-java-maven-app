037c2fc38af93f0a1e6a749d0689166d61c91e0e
Kurt Madel <kmadel@cloudbees.com>
2020-08-27 11:05:51 -0400

    Update App.java
    Fixed indentation to match google java sytles

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,16 +5,15 @@ package com.mycompany.app;
  */
 public class App {
 
-    private final String message = "Hello World!";
+  private final String message = "Hello World!";
 
-    public App() {}
+  public App() {}
 
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getMessage());
+  }
 
-    private final String getMessage() {
-        return message;
-    }
-
+  private final String getMessage() {
+    return message;
+  }
 }
